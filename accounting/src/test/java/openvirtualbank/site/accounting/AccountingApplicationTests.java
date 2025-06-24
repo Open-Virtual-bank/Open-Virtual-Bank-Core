af947370f25dc407849b4f9e89ad31846ad5c0be
greensnapback0229 <smdmim@gmail.com>
2025-06-24 09:50:45 +0900

    project init

---
+++ accounting/src/test/java/openvirtualbank/site/accounting/AccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package openvirtualbank.site.accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ orchestration/src/test/java/openvirtualbank/site/orchestration/OrchestrationApplicationTests.java
@@ -0,0 +1,13 @@
+package openvirtualbank.site.orchestration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
