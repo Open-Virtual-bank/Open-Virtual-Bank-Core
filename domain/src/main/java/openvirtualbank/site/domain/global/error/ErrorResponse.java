f4e19747f29f5ce47e63d588deb807ac0ef677f0
jeonkyujin <954qew@naver.com>
2025-06-28 17:51:01 +0900

    feat($Domain) : 공통 ErrorCode 및 ErrorResponse 정의

---
+++ domain/src/main/java/openvirtualbank/site/domain/global/error/ErrorResponse.java
@@ -0,0 +1,58 @@
+package openvirtualbank.site.domain.global.error;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.*;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private String code;
+    private String message;
+    private String method;
+    private String requestURI;
+    private List<FieldError> errors;
+
+    @Builder
+    public ErrorResponse(ErrorCode errorCode, HttpServletRequest request, List<FieldError> errors) {
+        this.timestamp = LocalDateTime.now();
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+        this.method = request.getMethod();
+        this.requestURI = request.getRequestURI();
+        this.errors = errors;
+    }
+
+    @Builder
+    public ErrorResponse(ErrorCode errorCode, HttpServletRequest request) {
+        this.timestamp = LocalDateTime.now();
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+        this.method = request.getMethod();
+        this.requestURI = request.getRequestURI();
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode, HttpServletRequest request, List<FieldError> errors) {
+        return new ErrorResponse(errorCode, request, errors);
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode, HttpServletRequest request) {
+        return new ErrorResponse(errorCode, request);
+    }
+
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class FieldError {
+        private final String field;
+        private final String reason;
+
+        public static FieldError of(FieldError fieldError) {
+            return new FieldError(fieldError.field, fieldError.reason);
+        }
+    }
+
+}
