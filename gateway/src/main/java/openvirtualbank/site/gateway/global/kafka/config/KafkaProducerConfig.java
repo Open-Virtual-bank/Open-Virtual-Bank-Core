68d7a2ea5e2e1469767f4f70404ed0db22336215
junbeom <jach1206@naver.com>
2025-06-28 20:05:03 +0900

    docs($gateway): Kafka config 주석 추가

--- gateway/src/main/java/openvirtualbank/site/gateway/global/kafka/config/KafkaProducerConfig.java
+++ gateway/src/main/java/openvirtualbank/site/gateway/global/kafka/config/KafkaProducerConfig.java
@@ -16,10 +16,10 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); //todo 배포시 배포 주소로 변경
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.ACKS_CONFIG, "all");
+        config.put(ProducerConfig.ACKS_CONFIG, "all"); // 브로커에서 메세지를 받고 그 메세지를 페일오버까지 받았을 경우 ack 수신 -> 안정성이 가장 높음
         config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
         return new DefaultKafkaProducerFactory<>(config);
     }
