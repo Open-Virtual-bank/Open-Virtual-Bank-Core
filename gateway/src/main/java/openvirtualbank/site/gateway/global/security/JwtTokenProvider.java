38d374d7808c362270e6bedd80acc549f847bc3c
greensnapback0229 <smdmim@gmail.com>
2025-06-28 23:48:16 +0900

    feat($Gateway): Jwt관련 provider 추가  - 생성, 검증, claim 파싱 기능

---
+++ gateway/src/main/java/openvirtualbank/site/gateway/global/security/JwtTokenProvider.java
@@ -0,0 +1,76 @@
+package openvirtualbank.site.gateway.global.security;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class JwtTokenProvider {
+
+	private SecretKey secretKey;
+
+	public JwtTokenProvider(@Value("${spring.jwt.secret}") String secret) {
+		secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+			Jwts.SIG.HS256.key().build().getAlgorithm());
+		System.out.println("Secret Key: " + createJwt("test", "Member", 3000L));
+	}
+
+	public String getId(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.get("id", String.class); // 👈 변경됨
+	}
+
+	public String getRole(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.get("role", String.class);
+	}
+
+	public Boolean isExpired(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.getExpiration()
+			.before(new Date());
+	}
+
+	public String createJwt(String id, String role, Long expiredMs) {
+		return Jwts.builder()
+			.claim("id", id)        // 👈 변경됨
+			.claim("role", role)
+			.issuedAt(new Date(System.currentTimeMillis()))
+			.expiration(new Date(System.currentTimeMillis() + expiredMs))
+			.signWith(secretKey)
+			.compact();
+	}
+
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parser()
+				.verifyWith(secretKey)
+				.build()
+				.parseSignedClaims(token);
+			return true;
+		} catch (JwtException | IllegalArgumentException e) {
+			return false;
+		}
+	}
+}
+    
