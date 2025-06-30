0c18b3877a6f96b142bed50e39e20c3615a5f66e
jeonkyujin <954qew@naver.com>
2025-06-30 23:45:48 +0900

    refactor($member): 이메일 검증관련 API 응답 형식 통일

--- member/src/main/java/openvirtualbank/site/member/join/controller/JoinMemberController.java
+++ member/src/main/java/openvirtualbank/site/member/join/controller/JoinMemberController.java
@@ -2,6 +2,7 @@ package openvirtualbank.site.member.join.controller;
 
 import java.security.NoSuchAlgorithmException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import openvirtualbank.site.domain.global.common.ApiResponse;
 import openvirtualbank.site.member.join.dto.request.EmailRequest;
 import openvirtualbank.site.member.join.dto.request.VerifyRequest;
 import openvirtualbank.site.member.join.dto.response.AuthNumberResponse;
@@ -25,15 +27,15 @@ public class JoinMemberController {
 	private final AuthMailService authMailService;
 
 	@PostMapping("/email/send-code")
-	public ResponseEntity<AuthNumberResponse> sendEmail(@RequestBody @Valid EmailRequest request) throws
+	public ResponseEntity<ApiResponse<AuthNumberResponse>> sendEmail(@RequestBody @Valid EmailRequest request) throws
 		NoSuchAlgorithmException {
 		AuthNumberResponse response = authMailService.sendCodeEmail(request.email());
-		return ResponseEntity.ok(response);
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
 	}
 
 	@GetMapping("/email/verify-code")
-	public ResponseEntity<VerifyResponse> verifyEmail(@RequestBody @Valid VerifyRequest request) throws Exception{
-		return ResponseEntity.ok(authMailService.verifyCode(request.uuid(), request.AuthNumber()));
+	public ResponseEntity<ApiResponse<VerifyResponse>> verifyEmail(@RequestBody @Valid VerifyRequest request) throws Exception{
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(authMailService.verifyCode(request.uuid(), request.AuthNumber())));
 	}
 
 
