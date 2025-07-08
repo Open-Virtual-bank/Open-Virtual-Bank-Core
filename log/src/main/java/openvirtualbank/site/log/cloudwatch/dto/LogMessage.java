6a8fe59d3e89c0c71586cec97d56b41599a20390
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2025-07-08 10:31:54 +0900

    feat($log): validate 코드 추가

--- log/src/main/java/openvirtualbank/site/log/cloudwatch/dto/LogMessage.java
+++ log/src/main/java/openvirtualbank/site/log/cloudwatch/dto/LogMessage.java
@@ -1,20 +1,24 @@
 package openvirtualbank.site.log.cloudwatch.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LogMessage {
-    private String logLevel;
-    private String timestamp;
-    private String callApiPath;
-    private String apiMethod;
-    private String txId;
-    private String srcIp;
-    private String deviceInfo;
-    private String memberId;
-    private String requestId;
-    private String srcChannel;
-    private String eventType;
-    private String ledgerCode;
-    private String errorMessage;
+	@NotBlank
+	private String logLevel;
+	@NotBlank
+	private String timestamp;
+	private String callApiPath;
+	private String apiMethod;
+	private String txId;
+	private String srcIp;
+	private String deviceInfo;
+	private String memberId;
+	private String requestId;
+	@NotBlank
+	private String srcChannel;
+	private String eventType;
+	private String ledgerCode;
+	private String errorMessage;
 }
